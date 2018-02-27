b1b6042e9b311818be203e350507e5c9fdc47b3f
Robin Deits <robin.deits@gmail.com>
2018-02-26 19:50:47 -0500

    add tests

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 [![Build Status](https://travis-ci.org/rdeits/MeshCatMechanisms.jl.svg?branch=master)](https://travis-ci.org/rdeits/MeshCatMechanisms.jl)
 
-[![Coverage Status](https://coveralls.io/repos/rdeits/MeshCatMechanisms.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/rdeits/MeshCatMechanisms.jl?branch=master)
-
 [![codecov.io](http://codecov.io/github/rdeits/MeshCatMechanisms.jl/coverage.svg?branch=master)](http://codecov.io/github/rdeits/MeshCatMechanisms.jl?branch=master)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,42 @@
 using MeshCatMechanisms
 using Base.Test
+using MeshCat: Visualizer, setobject!, settransform!
+using RigidBodyDynamics: Mechanism, MechanismState, parse_urdf, set_configuration!
+using CoordinateTransformations: Translation
+using ValkyrieRobot: Valkyrie
 
-# write your own tests here
-@test 1 == 2
+vis = Visualizer()
+
+if !haskey(ENV, "CI")
+	open(vis)
+	wait(vis)
+end
+
+@testset "acrobot" begin
+	urdf = "urdf/Acrobot.urdf"
+	robot = parse_urdf(Float64, urdf)
+	mvis = MechanismVisualizer(robot, urdf, vis[:acrobot, :robot])
+	state = MechanismState(robot)
+	set_configuration!(state, [1.0, -0.5])
+	setstate!(mvis, state)
+end
+
+@testset "acrobot with fixed elbow" begin
+	urdf = "urdf/Acrobot_fixed.urdf"
+	robot = parse_urdf(Float64, urdf)
+	mvis = MechanismVisualizer(robot, urdf, vis[:acrobot_fixed, :robot])
+	settransform!(vis[:acrobot_fixed], Translation(0, 1, 0))
+	state = MechanismState(robot)
+	set_configuration!(state, [1.0])
+	setstate!(mvis, state)
+end
+
+@testset "valkyrie" begin
+	val = Valkyrie();
+	mvis = MechanismVisualizer(val.mechanism, ValkyrieRobot.urdfpath(), vis[:valkyrie, :robot], 
+	                           package_path=[dirname(dirname(ValkyrieRobot.urdfpath()))])
+	settransform!(vis[:valkyrie], Translation(0, 2, 1))
+	state = MechanismState(val.mechanism)
+	setstate!(mvis, state)
+end
+
