9ae6cb78fbe11614d50ca573179b40586264b50f
Robin Deits <robin.deits@gmail.com>
2018-05-24 16:05:44 -0500

    make it easier to visualize coordinate frames and other geometries

--- src/visualizer.jl
+++ src/visualizer.jl
@@ -16,29 +16,52 @@ end
 
 MechanismVisualizer(m::Mechanism, args...) = MechanismVisualizer(MechanismState{Float64}(m), args...)
 
+mechanism(mvis::MechanismVisualizer) = mvis.state.mechanism
+
 to_affine_map(tform::Transform3D) = AffineMap(rotation(tform), translation(tform))
 
 function _set_mechanism!(mvis::MechanismVisualizer, elements::AbstractVector{<:VisualElement})
     for (i, element) in enumerate(elements)
-        _set_element!(mvis, element, "geometry_$i")
+        setelement!(mvis, element, "geometry_$i")
     end
 end
 
-function _set_element!(mvis::MechanismVisualizer, element::VisualElement, name::AbstractString)
-    mechanism = mvis.state.mechanism
-    vis = mvis.visualizer
-    tree = mechanism.tree
-    # TODO: much of this information can be cached if this
-    # method becomes a performance bottleneck
-    body = rbd.body_fixed_frame_to_body(mechanism, element.frame)
-    body_ancestors = rbd.Graphs.ancestors(body, tree)
-    path = vcat(string.(reverse(body_ancestors)), string(element.frame))
-    frame_vis = vis[path...]
-    definition = rbd.frame_definition(body, element.frame)
-    settransform!(frame_vis, to_affine_map(definition))
-    setobject!(frame_vis[name], element.geometry, MeshLambertMaterial(color=element.color))
-    settransform!(frame_vis[name], element.transform)
+Base.getindex(mvis::MechanismVisualizer, x...) = getindex(mvis.visualizer, x...)
+
+# TODO: much of this information can be cached if this
+# method becomes a performance bottleneck.
+# We can probably just put `@memoize` from Memoize.jl right here.
+function Base.getindex(mvis::MechanismVisualizer, frame::CartesianFrame3D)
+    body = rbd.body_fixed_frame_to_body(mechanism(mvis), frame)
+    mvis[body][string(frame)]
 end
+
+function Base.getindex(mvis::MechanismVisualizer, body::RigidBody)
+    path = _path(mechanism(mvis), body)
+    mvis[path...]
+end
+
+function setelement!(mvis::MechanismVisualizer, element::VisualElement, name::AbstractString="<element>")
+    setelement!(mvis, element.frame, element.geometry, MeshLambertMaterial(color=element.color), name)
+    settransform!(mvis[element.frame][name], element.transform)
+end
+
+function setelement!(mvis::MechanismVisualizer, frame::CartesianFrame3D, object::AbstractObject, name::AbstractString="<element>")
+    body = rbd.body_fixed_frame_to_body(mechanism(mvis), frame)
+    definition = rbd.frame_definition(body, frame)
+    frame_vis = mvis[frame]
+    settransform!(frame_vis, to_affine_map(definition))
+    setobject!(frame_vis[name], object)
+end
+
+setelement!(mvis::MechanismVisualizer, frame::CartesianFrame3D, geometry::GeometryLike, args...) = setelement!(mvis, frame, Object(geometry), args...)
+setelement!(mvis::MechanismVisualizer, frame::CartesianFrame3D, geometry::GeometryLike, material::AbstractMaterial, args...) = setelement!(mvis, frame, Object(geometry, material), args...)
+
+function _path(mechanism, body)
+    body_ancestors = ancestors(body, mechanism.tree)
+    path = string.(reverse(body_ancestors))
+end
+
 
 function _render_state!(mvis::MechanismVisualizer, state::MechanismState=mvis.state)
     @assert mvis.state.mechanism === state.mechanism
@@ -48,14 +71,13 @@ function _render_state!(mvis::MechanismVisualizer, state::MechanismState=mvis.st
     mechanism = mvis.state.mechanism
     vis = mvis.visualizer
     tree = mechanism.tree # TODO: tree accessor?
-    for vertex in rbd.Graphs.vertices(tree)
-        body = vertex
-        if body == rbd.Graphs.root(tree)
+    for body in vertices(tree)
+        if body == root(tree)
             settransform!(vis, to_affine_map(transform_to_root(state, body)))
         else
-            body_ancestors = rbd.Graphs.ancestors(vertex, tree)
-            path = string.(reverse(body_ancestors))
-            tform = relative_transform(state, default_frame(body), default_frame(body_ancestors[2]))
+            path = _path(mechanism, body)
+            parent = source(edge_to_parent(body, mechanism.tree), mechanism.tree)
+            tform = relative_transform(state, default_frame(body), default_frame(parent))
             settransform!(vis[path...], to_affine_map(tform))
         end
     end
