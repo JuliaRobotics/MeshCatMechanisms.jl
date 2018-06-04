2f188558aef47c61439c82612416275c646238cf
Robin Deits <robin.deits@gmail.com>
2018-06-04 00:07:49 -0400

    very basic animation support for vectors of times and configurations

--- src/animate.jl
+++ src/animate.jl
@@ -29,3 +29,38 @@ function animate(vis::MechanismVisualizer,
         t == tf && break
     end max_rate = fps
 end
+
+using MeshCat: AnimationClip, TransformTrack, Animation, SetAnimation, Path
+
+function setanimation(mvis::MechanismVisualizer,
+                      times::Vector{Float64},
+                      configurations::AbstractVector{<:AbstractVector{<:Real}},
+                      fps::Integer=30)
+    state = MechanismState(mechanism(mvis))
+    clips = Dict{Path, AnimationClip{TransformTrack}}()
+    tree = mechanism(mvis).tree
+    @assert indices(times) == indices(configurations)
+    interpolated_configurations = interpolate((times,), configurations, Gridded(Linear()))
+    num_frames = floor(Int, (times[end] - first(times)) * fps)
+    for frame in 0:num_frames
+        time = first(times) + frame / fps
+        set_configuration!(state, interpolated_configurations[time])
+        for body in vertices(tree)
+            if body === root(tree)
+                continue
+            end
+            parent = source(edge_to_parent(body, tree), tree)
+            tform = to_affine_map(relative_transform(state, default_frame(body), default_frame(parent)))
+            path = mvis[body].path
+            clip = get!(clips, path) do
+                AnimationClip{TransformTrack}(tracks=[TransformTrack([])], fps=fps)
+            end
+            track = first(clip.tracks)
+            push!(track.frames, frame => tform)
+        end
+    end
+    anim = Animation(collect(clips))
+    cmd = SetAnimation(anim, true, 1)
+    send(mvis.visualizer.core, cmd)
+    return cmd
+end
